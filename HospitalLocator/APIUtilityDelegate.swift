dbf210d3f4747617c8043fbe0241cea7b6637d0d
Mikhail Sapozhnikov <mikhailsapozhnikov@Mikhails-MacBook-Pro.local>
2016-05-26 22:15:02 -0400

    Update readme with refderences.

---
+++ HospitalLocator/APIUtilityDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  APIUtilityDelegate.swift
+//  HospitalLocator
+//
+//  Created by Mikhail Sapozhnikov on 5/26/16.
+//  Copyright © 2016 Mikhail Sapozhnikov. All rights reserved.
+//
+
+import Foundation
