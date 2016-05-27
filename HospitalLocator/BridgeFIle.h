73054f188143af52bca0056635e76f86001910d9
Mikhail Sapozhnikov <mikhailsapozhnikov@Mikhails-MacBook-Pro.local>
2016-05-27 01:54:57 -0400

    Add component for HockeySDK

---
+++ HospitalLocator/BridgeFIle.h
@@ -0,0 +1,13 @@
+//
+//  BridgeFIle.h
+//  HospitalLocator
+//
+//  Created by Mikhail Sapozhnikov on 5/27/16.
+//  Copyright © 2016 Mikhail Sapozhnikov. All rights reserved.
+//
+
+#ifndef BridgeFIle_h
+#define BridgeFIle_h
+
+
+#endif /* BridgeFIle_h */
